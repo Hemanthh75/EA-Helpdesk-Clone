dd24c620d3fb4d924eddc2823ebea4ffa02a0110
Superkings@555 <hemanthpavan555@gmail.com>
2024-11-12 22:36:09 +0530

    12/11 Work

--- client/vite-project/src/components/GameSupport/GameSupport.jsx
+++ client/vite-project/src/components/GameSupport/GameSupport.jsx
@@ -5,15 +5,22 @@ const GameSupport = () => {
   return (
     <div className='game-support-container'>
         <div className='game-support-input-filter'>
-            <input type="text" className='game-support-input' placeholder='Search for a game or product' />
-            <div className='game-support-filter'>
-              <p>View by:</p>
-              <select>
-                  <option>Recommended</option>
-                  <option>A To Z</option>
-                  <option>Z TO A</option>
-                  <option>Oldest</option>
-                  <option>Newest</option>
+
+
+            <div className='game-support-input-container'>
+              {/*game support input field*/}
+              <input type="text" className='game-support-input' placeholder='Search for a game or product' />
+            </div>
+
+            {/*game support dropdown*/}
+            <div className='game-support-filter-container'>
+              <p className='game-support-filter-text'>View by:</p>
+              <select className='game-support-filter-dropdown'>
+                  <option className='game-support-filter-dropdown-item'>Recommended</option>
+                  <option className='game-support-filter-dropdown-item'>A To Z</option>
+                  <option className='game-support-filter-dropdown-item'>Z TO A</option>
+                  <option className='game-support-filter-dropdown-item'>Oldest</option>
+                  <option className='game-support-filter-dropdown-item'>Newest</option>
               </select>
             </div>
         </div>
