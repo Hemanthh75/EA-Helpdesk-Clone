7764c178ecc0c2183906d6ff52902d93863c2d44
Superkings@555 <hemanthpavan555@gmail.com>
2024-11-24 20:54:27 +0530

    24/11 work

--- client/vite-project/src/pages/SignIn/SignIn.jsx
+++ client/vite-project/src/pages/SignIn/SignIn.jsx
@@ -16,11 +16,36 @@ const SignIn = () => {
                     <div className="social-logins">
                         <img src="/Images/Signin/Google.png" alt="" className="social-login-logo" />
                         <img src="/Images/Signin/fb.png" alt="" className="social-login-logo"/>
+                        <img src="/Images/Signin/apple.png" alt="" className="social-login-logo"/>
                         <img src="/Images/Signin/Steam.png" alt="" className="social-login-logo"/>
                         <img src="/Images/Signin/xbox.png" alt="" className="social-login-logo"/>
                         <img src="/Images/Signin/ps.png" alt="" className="social-login-logo"/>
                     </div>
+                    
+                    <div className="social-logins-divider">
+                       <span>or</span>
+                    </div>
+                   
                 </div>
+                
+                <form action="" className="signin-form">
+                    <label className="sigin-phone-email-label">PHONE OR EMAIL</label>
+                    <input type="text" placeholder="Enter your phone or email" className="sigin-input"/>
+                    <div className="sigin-checkbox">
+                        <input type="checkbox"  className="signin-checkbox-input"/>
+                        <label>Remember Me</label>
+                    </div>
+
+                    <button className="sigin-page-next-btn">NEXT</button>
+                    
+                </form>
+
+                <div className="sigin-forgot-newacc">
+                    <a href="" >Forgot yout password, or need to create a new one?</a>
+                </div>
+                
+                <button className="sigin-in-page-create-btn">CREATE ACCOUNT</button>
+
             </div>
 
         </div>
