18c01386874b35a620ea31f89fb1dd0f95094a22
Potineedi <hpotineedi@contractor.ea.com>
2025-01-08 19:32:24 +0530

    Issue fixes

--- client/vite-project/src/pages/MyCases/MyCases.jsx
+++ client/vite-project/src/pages/MyCases/MyCases.jsx
@@ -1,40 +1,48 @@
 import React from "react";
-import './mycases.css'
+import "./mycases.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
 const MyCases = () => {
-    return (
-            <>
-                <Navbar />
+  return (
+    <>
+      <Navbar />
 
-                {/* Creating the my cases page container */}
-               <div className="my-cases-container">
-                    <div className="my-cases-content">
-                        <div className="my-cases-content-description">
-                            <p className="my-cases-content-description-text">Please sign into your EA Account to proceed.</p>
-                            <img className="my-cases-content-description-Image" src="/Images/MyCases/EALogo.png" alt="" />
-                            <button className="my-cases-content-description-btn">Log in</button>
-                        </div>
+      {/* Creating the my cases page container */}
+      <div className="my-cases-container">
+        <div className="my-cases-content">
+          <div className="my-cases-content-description">
+            <p className="my-cases-content-description-text">
+              Please sign into your EA Account to proceed.
+            </p>
+            <img
+              className="my-cases-content-description-Image"
+              src="/Images/MyCases/EALogo.png"
+              alt=""
+            />
+            <button className="my-cases-content-description-btn">Log in</button>
+          </div>
 
-                {/* Creating the my cases page input form */}
-                        <div className="my-cases-content-inputs">
-                            <h2 className="my-cases-content-inputs-heading">Access an Individual Case:</h2>
-                            <form>
-                                <label className="mycaseslabel">Case ID</label>
-                                <input type="text" name="caseid"/>
-                                <label className="mycaseslabel">Access Key</label>
-                                <input type="text" name="accesskey" />
-                                <button type="submit" className="my-cases-content-inputs-btn">View Case</button>
-                            </form>
-                        </div>
-                   </div>
-
-                </div>
-                <Footer />
-            </>
-      
-    )
-}
+          {/* Creating the my cases page input form */}
+          <div className="my-cases-content-inputs">
+            <h2 className="my-cases-content-inputs-heading">
+              Access an Individual Case:
+            </h2>
+            <form>
+              <label className="mycaseslabel">Case ID</label>
+              <input type="text" name="caseid" />
+              <label className="mycaseslabel">Access Key</label>
+              <input type="text" name="accesskey" />
+              <button type="submit" className="my-cases-content-inputs-btn">
+                View Case
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default MyCases;
